21a6973676c3740aa5b7fc93903a983220f03ed1
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-28 13:48:57 +0300

    add sources class template

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Sources:
+    """
+    class that defines news sources objects 
+    """
+    def __init__(self,id,name, description, category, url, country,author):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.url = url
+        self.country = country
+        self.author = author
+
+
+    
+
